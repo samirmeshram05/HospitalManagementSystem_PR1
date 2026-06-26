1b35ff99be08cea53eef5168b5f3e59220209bc8
samirmeshram05 <134682176+samirmeshram05@users.noreply.github.com>
2026-06-27 00:58:16 +0530

    feat: integrate Swagger OpenAPI documentation

--- src/main/java/com/hospital/config/OpenApiConfig.java
+++ src/main/java/com/hospital/config/OpenApiConfig.java
@@ -1,29 +1,32 @@
 package com.hospital.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class SwaggerConfig {
-// http://localhost:8080/swagger-ui/index.html#/
+public class OpenApiConfig {
+
+	// http://localhost:8080/swagger-ui/index.html#/
     @Bean
     public OpenAPI hospitalOpenAPI() {
-
         return new OpenAPI()
                 .info(new Info()
                         .title("Hospital Management System API")
-                        .description("Spring Boot REST API for Hospital Management System")
+                        .description("Hospital Management System using Spring Boot, JPA, MySQL, JWT Authentication")
                         .version("1.0")
                         .contact(new Contact()
                                 .name("Samir Sindhimeshram")
                                 .email("samirmeshram47@gmail.com")))
+                
                 .externalDocs(new ExternalDocumentation()
                         .description("Project Documentation")
                         .url("https://github.com/"));
+
     }
 
 }
